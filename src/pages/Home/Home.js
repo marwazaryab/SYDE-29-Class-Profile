14af8ed4157421012012d160c5b5316539b091fc
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-02-10 22:08:03 -0500

    update home page styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,23 +5,19 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="background">
-
         <div className="glow-green"></div>
         <div className="glow-pink"></div>
         <div className="glow-blue"></div>
-
       </div>
-
+      
       <div className="content">
-        <h1>
-          A LOOK IN SYDE CLASS OF '29!
-        </h1>
+        <h2 className="top-left">A look in</h2>
+        <h3 className="middle">SYDE</h3>
+        <h2 className="bottom">Class of '29</h2>
         <button className="button">LET'S GO!</button>
       </div>
-
     </div>
   );
 };
 
 export default Home;
-
