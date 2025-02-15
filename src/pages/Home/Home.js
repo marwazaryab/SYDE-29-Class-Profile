21073255e7cf74d632009a0c41d6a00ae918f61e
Nandika Patel <nandika.patel@shopify.com>
2025-02-14 22:52:21 -0500

    Formatted Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,9 +11,10 @@ const Home = () => {
       </div>
       
       <div className="content">
-        <h2 className="top-left">A look in</h2>
-        <h3 className="middle">SYDE</h3>
-        <h2 className="bottom">Class of '29</h2>
+        <h2 className="top-left">A LOOK</h2>
+        <h2 className="middle-left">IN</h2>
+        <h3 className="middle-right">SYDE</h3>
+        <h2 className="bottom">CLASS OF '29!</h2>
         <button className="button">LET'S GO!</button>
       </div>
     </div>
